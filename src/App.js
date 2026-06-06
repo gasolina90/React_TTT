4f464c6e76be1750c50b5da8f56b701177bc17ee
Erick <moralerick90@gmail.com>
2026-06-05 20:20:20 -0700

    clickable board

--- src/App.js
+++ src/App.js
@@ -1,3 +1,40 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+function Square({value}) {
+  const [value, setValue] = useState(null)
+
+  function handleClick() {
+    setValue("X")
+  }
+
+  return (
+    <buton
+    className="square"
+    onClick={handleClick}
+    >
+      {value}
+    </buton>
+  );
+}
+
+export default function board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
